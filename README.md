ede9cb8e487937b4aeeb14b72b47dfedea26db19
Robin LOUARN <robin.louanrn@icloud.com>
2019-04-28 21:33:24 +0200

    modification du script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # DockerExemple
+
+## Lancement
+
+Pour lancer le server executer le script.
+Il permet la creation des volumes et la creation du gitignore.
+
+## Conposition du docker compose
+
+- jenkins
+- sonarqube
+- tomcat
+- phpmyadmin
+- mysql
+- potainer
--- script.sh
+++ script.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-mkdir -p ./jenkins-volume
+volumes="jenkins-volume mysql-volume portainer-volume"
+gitignore=".gitignore"
 
-#docker-compose -f docker-compose.yml up -d --remove-orphans
+echo "idea" > $gitignore
+
+for volume in $volumes
+do
+    if ! test -d $volume
+    then
+      mkdir -p ./$volume
+    fi
+    echo $volume >> $gitignore
+done
+
+docker-compose -f docker-compose.yml up -d
 
