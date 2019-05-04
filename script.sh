fbc0c5178126a319060a63b45a70553ce59e3b63
Robin LOUARN <robin.louarn@icloud.com>
2019-05-04 21:47:29 +0200

    modification des stacks

--- script.sh
+++ script.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-volumes="jenkins-volume mysql-volume portainer-volume"
-gitignore=".gitignore"
+## creation des dossiers des volumes
+#volumes="jenkins-volume mysql-volume portainer-volume"
+#gitignore=".gitignore"
+#
+#echo "idea" > $gitignore
+#
+#for volume in $volumes
+#do
+#    if ! test -d $volume
+#    then
+#      mkdir -p ./$volume
+#    fi
+#    echo $volume >> $gitignore
+#done
 
-echo "idea" > $gitignore
-
-for volume in $volumes
-do
-    if ! test -d $volume
-    then
-      mkdir -p ./$volume
-    fi
-    echo $volume >> $gitignore
-done
-
-docker-compose up -d
+## Exemple de commandes
+#docker-compose stop
+#docker-compose rm
+#docker-compose ps
+#docker-compose up -d
+#docker-compose down --remove-orphans
+#docker-compose up -d --remove-orphans
 
